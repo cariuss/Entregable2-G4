91805c46bde24a870a079ceb94fb0f50680b5dac
cariuss <guillentcarlos@gmail.com>
2025-06-04 22:43:17 -0500

    Ignorando __pycache__ y archivos .pyc

--- gestor_de_recursos/apps/usuarios/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuario
-
-@admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ("id", "nombre", "correo", "numero", "direccion", "rol")
-    search_fields = ("nombre", "correo", "numero")
-    list_filter = ("rol",)
